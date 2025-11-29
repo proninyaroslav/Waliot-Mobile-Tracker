49b432187bcdee55f470796d0a8278b564823a1e
Yaroslav Pronin <proninyaroslav@gmail.com>
2025-11-29 09:58:59 +0300

    Add splash screen api

--- phoneClients/android/app/src/main/java/com/websmithing/gpstracker2/MainActivity.kt
+++ phoneClients/android/app/src/main/java/com/websmithing/gpstracker2/MainActivity.kt
@@ -6,13 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.websmithing.gpstracker2.ui.App
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
